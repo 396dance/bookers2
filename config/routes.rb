23dbbb062988a676598d7f3880e32e3d6789c3e7
EC2 Default User <ec2-user@ip-172-31-11-134.ap-northeast-1.compute.internal>
2022-08-18 14:49:56 +0000

    [add]ログイン機能、bookのviewページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+
+  get '/homes/about' => 'homes#about', as:'about'
+
+
+  resources :books, only: [:index, :show, :edit]
+  post 'books' => 'books#create'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+
+
 end
