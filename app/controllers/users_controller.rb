eb933f7d6eda9605e79587a262e9ed704e7d5f46
EC2 Default User <ec2-user@ip-172-31-11-134.ap-northeast-1.compute.internal>
2022-08-21 10:54:42 +0000

    [add]books/show作成完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,36 @@
 class UsersController < ApplicationController
   def index
-    @books = Book.all
+    @users = User.all
+    @user = current_user
+    # @books = Book.all
   end
 
   def show
     @user = User.find(params[:id])
+    # bookモデルの情報を投稿するための投稿フォーム作成のために空のオブジェクト生成
     @book = Book.new
     @books = @user.books
   end
 
   def edit
+    # userテーブルにあるidが1のレコードを取得とか
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
